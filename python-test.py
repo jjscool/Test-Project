5c72a83b8e96ac8beba29e9cee37e5ddde67a443
jjscool <siebert.j@web.de>
2022-01-30 08:20:36 +0100

    Init

---
+++ python-test.py
@@ -0,0 +1,7 @@
+"""greeting="hello world\n"
+print(f"{greeting.title()} how are you")
+print("This is a new line") """
+mylist=['dog','cat','horse']
+for animal in mylist:
+ print(f"{animal}\n")
+print("this is the end")
